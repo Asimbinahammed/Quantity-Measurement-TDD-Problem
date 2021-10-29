9fb1be503cf0387e46576c132f7369370192febf
asimbinahammed <asim242370@gmail.com>
2021-10-29 15:13:56 +0530

    Added:Test cases of type checking

--- src/main/java/com/bridglab/UnitConversion.java
+++ src/main/java/com/bridglab/UnitConversion.java
@@ -7,7 +7,7 @@ package com.bridglab;
 public class UnitConversion {
 
     /*
-    * Returning feet values if it gets input 
+    * Returning feet values if it gets input
     */
     public <object> Object feetDeclare(object o) {
         return o;
--- src/test/java/com/bridgelabz/UnitConversionTest.java
+++ src/test/java/com/bridgelabz/UnitConversionTest.java
@@ -7,23 +7,51 @@ import org.junit.jupiter.api.Test;
 public class UnitConversionTest {
     UnitConversion unitConversion =new UnitConversion();
 
+    /**
+     * Checking same different feet retuns false
+     */
     @Test
     public void whenGivenWrongFeetValueShouldFailTest() {
         Assertions.assertEquals(unitConversion.feetDeclare(0),unitConversion.feetDeclare(1));
     }
 
+    /**
+     * Checking same vlue of fee returns true
+     */
     @Test
     public void whenGivenRightFeetValueShouldPassTest() {
         Assertions.assertEquals(unitConversion.feetDeclare(0),unitConversion.feetDeclare(0));
     }
 
+    /**
+     * Checkingone null value with normal value
+     */
     @Test
     public void whenGivenNullValueShouldFailTest() {
         Assertions.assertEquals(unitConversion.feetDeclare(null),unitConversion.feetDeclare(1));
     }
 
+    /*
+    checking null value of feet with null value
+     */
     @Test
     public void whenGivenNullValueShouldPassTest() {
         Assertions.assertEquals(unitConversion.feetDeclare(null),unitConversion.feetDeclare(null));
+    }
+
+    /**
+     * Checking int value with double value
+     */
+    @Test
+    public void whenGivenIntValueAndDoubleValueShouldFailTest() {
+        Assertions.assertEquals(unitConversion.feetDeclare(1.0),unitConversion.feetDeclare(1));
+    }
+
+    /**
+     * Checking double value with double value
+     */
+    @Test
+    public void whenGivenDoubleValueAndDoubleValueShouldPassTest() {
+        Assertions.assertEquals(unitConversion.feetDeclare(1.0),unitConversion.feetDeclare(1.0));
     }
 }
