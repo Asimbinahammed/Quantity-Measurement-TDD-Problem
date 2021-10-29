88e3759b926952a1c8976187903ae3c8707dfa90
asimbinahammed <asim242370@gmail.com>
2021-10-29 14:39:06 +0530

    Added:Testing feet with feet values

--- src/main/java/com/bridglab/UnitConversion.java
+++ src/main/java/com/bridglab/UnitConversion.java
@@ -1,8 +1,15 @@
+/*
+@author : ASIM AHAMMED
+ */
+
 package com.bridglab;
 
 public class UnitConversion {
 
-    public static void main(String[] args) {
-        System.out.println("Welcome to Quantity Measurement TDD Problem");
+    /*
+    * Returning feet values if it gets input integer
+    */
+    public int feetDeclare(int i) {
+        return i;
     }
 }
---
+++ src/test/java/com/bridgelabz/UnitConversionTest.java
@@ -0,0 +1,19 @@
+package com.bridgelabz;
+
+import com.bridglab.UnitConversion;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class UnitConversionTest {
+    UnitConversion unitConversion =new UnitConversion();
+
+    @Test
+    public void whenGivenWrongFeetValueShouldFailTest() {
+        Assertions.assertEquals(unitConversion.feetDeclare(0),unitConversion.feetDeclare(1));
+    }
+
+    @Test
+    public void whenGivenRightFeetValueShouldPassTest() {
+        Assertions.assertEquals(unitConversion.feetDeclare(0),unitConversion.feetDeclare(0));
+    }
+}
